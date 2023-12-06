fcb5e62c48fe930823dd9778482f65b73993b05d
Aldrin Jenson <aldrin.jenson@thoughtworks.com>
2023-12-06 20:17:20 +0530

    [Aldrin|Arul] Refactor. Remove unnecessary println

--- src/main/java/com/booking/movieGateway/MovieController.java
+++ src/main/java/com/booking/movieGateway/MovieController.java
@@ -36,7 +36,6 @@ public class MovieController {
     public ResponseEntity<List<Movie>> fetchMovies() {
         try {
             List<Movie> movies = movieGateway.getAllMovies();
-            System.out.println(movies.size());
             return new ResponseEntity<>(movies, HttpStatus.OK);
         } catch (IOException | FormatException e) {
             // Handle exception appropriately, you might want to log the error
